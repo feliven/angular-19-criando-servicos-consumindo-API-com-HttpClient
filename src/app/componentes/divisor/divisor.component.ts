5f127edc7553d6e08fc7837a2b443d50289455d9
FVS <feliven@gmail.com>
2025-11-11 13:01:04 -0300

    first commit

---
+++ src/app/componentes/divisor/divisor.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-divisor',
+  imports: [CommonModule],
+  templateUrl: './divisor.component.html',
+  styleUrl: './divisor.component.css',
+})
+export class DivisorComponent {
+  tamanho = input<'linha-destaque--grande' | 'linha-destaque--pequena'>(
+    'linha-destaque--grande'
+  );
+  classeGenero = input<string>('');
+}
