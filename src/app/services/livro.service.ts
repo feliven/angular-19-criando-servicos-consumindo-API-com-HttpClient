1fb6724c079ab8d586844e9dff98a2296bd621ef
FVS <feliven@gmail.com>
2025-11-11 13:18:43 -0300

    criado LivroService

---
+++ src/app/services/livro.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Livro } from '../componentes/livro/livro';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LivroService {
+  private enderecoAPI = 'http://localhost:3000/livros';
+
+  constructor(private http: HttpClient) {}
+
+  public get livros() {
+    return this.http.get<Livro[]>(this.enderecoAPI);
+  }
+}
